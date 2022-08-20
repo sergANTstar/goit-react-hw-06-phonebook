848091641cf2f95c635b65bb42150686bba6d461
sergANTstar <106014778+sergANTstar@users.noreply.github.com>
2022-08-20 17:20:09 +0300

    u

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,35 +1,40 @@
 import css from '../ContactForm/ContactForm.module.css';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 
-export class ContactForm extends Component {
-    state = {
-        name: '',
-        number: '',
-      };
+export function ContactForm ({contactFormSubmit}) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
     
-      inputChange = (e) => {
-        const { value, name } = e.target;
-        this.setState({ [name]: value });
-      };
+      const inputChange = (e) => {
+        const { name, value } = evt.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        break;
+    }
+  };
     
-      formSubmit = (e) => {
+      const formSubmit = (e) => {
         e.preventDefault();
-        const { name } = this.state;
-        const nameToLowerCase = name.toLowerCase();
-        this.props.onSubmit({ ...this.state, name: nameToLowerCase });
-        this.setState({ name: '', number: '' });
+        contactFormSubmit(name, number);
+        setName('');
+        setNumber('');
       };
     
-    render() {
-        const {name, number} = this.state;
         return(
-            <form onSubmit={this.formSubmit} className={css.contact__form}>
+            <form onSubmit={formSubmit} className={css.contact__form}>
             <input
               type="text"
               name="name"
               value={name}
-              onChange={this.inputChange}
+              onChange={inputChange}
               placeholder="Name"
               className={css.contact__input}
               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
@@ -41,10 +46,10 @@ export class ContactForm extends Component {
               name="number"
               value={number}
               placeholder="Number"
-              onChange={this.inputChange}
+              onChange={inputChange}
               className={css.contact__input}
               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               required
             />
             <button className={css.contact__button} type="submit">Add contact</button>
